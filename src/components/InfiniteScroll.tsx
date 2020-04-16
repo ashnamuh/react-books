0892434cac978854357b438662751ca5f78f1f7d
Ash <ashnamuh@gmail.com>
2020-04-16 21:32:51 +0900

    implement a InfiniteScroll component

---
+++ src/components/InfiniteScroll.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+
+import { throttle } from 'lodash'
+
+interface Props {
+  handler: Function;
+}
+
+export default function InfiniteScroll({ handler }: Props) {
+  const ref = React.useRef<HTMLDivElement>(null)
+
+  React.useEffect(() => {
+    const scrollHandler = throttle(() => {
+      if (ref.current?.getBoundingClientRect().top as number < window.innerHeight + 100) {
+        handler()
+      }
+    }, 100)
+
+    if (ref.current) {
+      window.addEventListener('scroll', scrollHandler)
+    }
+
+    return () => {
+      ref.current?.removeEventListener('input', scrollHandler) // eslint-disable-line
+    }
+  }, [ref, handler])
+
+  return (<div ref={ref} />)
+}
